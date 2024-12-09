a6fe38ddb8670f26bb320043c6144cedaddbcf96
Gabriel Alves Silva <ggalvesilva63@gmail.com>
2024-12-09 15:02:24 -0300

    Added endpoints to refresh and verify access tokens.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenRefreshView
 
 urlpatterns = [
     path('authentication/token/', TokenObtainPairView.as_view(), name='obtain_pair_view'),
+    path('authentication/token/refresh/', TokenRefreshView.as_view(), name='refresh_view'),
+    path('authentication/token/verify/', TokenVerifyView.as_view(), name='verify_view'),
 ]
