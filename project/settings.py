85d95f397bd8e3fe0ab9321065a0ee36a5f378eb
Gabriel Alves Silva <ggalvesilva63@gmail.com>
2024-12-09 14:28:51 -0300

    Created authentication app and defined url to generate token with simple-jwt

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'rest_framework',
+    'rest_framework_simplejwt',
+    
+    'authentication.apps.AuthenticationConfig',
     'genres.apps.GenresConfig',
     'actors.apps.ActorsConfig',
     'movies.apps.MoviesConfig',
@@ -130,3 +134,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
