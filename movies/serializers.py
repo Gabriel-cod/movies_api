c4dfa9311ad516ef9c0630e4a84ecd843be1df81
Gabriel Alves Silva <ggalvesilva63@gmail.com>
2024-12-12 07:23:56 -0300

    Created distincts serializer for get and post movies and applied it

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,24 +1,33 @@
 from django.db.models import Avg
 from rest_framework import serializers
+from actors.serializers import ActorSerializer
+from genres.serializers import GenreSerializer
 from .models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
 
-    def get_rate(self, obj):
-        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
-
-        if rate:
-            return round(rate, 1)
-
-        return None
-
     def validate_realease_date(self, value):
         if value.year < 1990:
             raise serializers.ValidationError("Movies older than 1990 are not allowed")
         return value
+
+
+class MovieListDetailSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+    genre = GenreSerializer()
+    actors = ActorSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'genre', 'actors', 'realease_date', 'resume', 'rate',]
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        if rate:
+            return round(rate, 1)
+        return None
--- movies/views.py
+++ movies/views.py
@@ -2,19 +2,27 @@ from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from project.permissions import GlobalPermissionClass
 from .models import Movie
-from .serializers import MovieSerializer
+from .serializers import MovieSerializer, MovieListDetailSerializer
 
 
 class MovieListCreateView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalPermissionClass,)
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return MovieListDetailSerializer
+        return MovieSerializer
 
 
 class MovieRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalPermissionClass,)
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return MovieListDetailSerializer
+        return MovieSerializer
 
 
 class MovieStatsView(views.APIView):
